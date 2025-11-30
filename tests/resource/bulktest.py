88ecb95f7f8177c0ee32ca4815f7239eb7c15121
GloriousGlider8 <sam.yt.gg8@gmail.com>
2025-11-30 14:25:26 +0000

    start test progress

--- tests/resource/bulktest.py
+++ tests/resource/bulktest.py
@@ -1,11 +1,14 @@
 from FFLResource import *
 import requests as r
-
-# find some path to ffl res data files
+import json as j
 
 middleRequest = r.get("https://gloriousglider8.github.io/assets/FFLResMiddle.dat")
 highRequest   = r.get("https://gloriousglider8.github.io/assets/FFLResHigh.dat")
 
+middleRequest.raise_for_status()
+highRequest.raise_for_status()
+
+middleDat = middleRequest.content
+highDat   = highRequest.content
 
 
-# TODO implement tests
---
+++ tests/resource/bulkwrite.py
@@ -0,0 +1,17 @@
+from FFLResource import *
+import requests as r
+import json as j
+
+middleRequest = r.get("https://gloriousglider8.github.io/assets/FFLResMiddle.dat")
+highRequest   = r.get("https://gloriousglider8.github.io/assets/FFLResHigh.dat")
+
+middleRequest.raise_for_status()
+highRequest.raise_for_status()
+
+middleDat = middleRequest.content
+highDat   = highRequest.content
+
+
+for res in [[middleDat, "middle"], [highDat, "high"]]:
+	#FFLResource
+	pass
