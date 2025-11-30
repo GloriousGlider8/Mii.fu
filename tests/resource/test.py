3327a8da3da239a4294d6ad56b184925d8c50f94
GloriousGlider8 <sam.yt.gg8@gmail.com>
2025-11-30 11:20:31 +0000

    use read-write references instead of passing by value

--- tests/resource/test.py
+++ tests/resource/test.py
@@ -3,7 +3,9 @@ import requests as r
 
 # find some path to ffl res data files
 
-resMiddleDat = r.get("").content
-resHighDat = r.get("").content
+middleRequest = r.get("https://gloriousglider8.github.io/assets/FFLResMiddle.dat")
+highRequest   = r.get("https://gloriousglider8.github.io/assets/FFLResHigh.dat")
+
+
 
 # TODO implement tests
